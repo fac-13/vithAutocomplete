c42f45b2c2e77424638eaacac17c605a150700e4
i2xzy <yitzchokr@live.com>
2018-03-22 17:12:06 +0000

    update logic and tests to use animals not dummy closes #22

--- src/logic.js
+++ src/logic.js
@@ -1,25 +1,22 @@
-const dummy = require('./animalsDummy.json');
 const animals = require('./animals.json');
 
 // PURE LOGICS FOR PROCESSING
 
 const logic = {
-	suggestions: input => {
+	searchSuggestions: input => {
         let regex = new RegExp(`\\b${input}`, 'g');
         console.log(regex);
 		let result = animals
 			.map(element => element['CommonName'])
             .filter(name => regex.test(name))
             .slice(0, 5)
-            .sort((a, b) => a > b); /// this isn't working needs to be sorted.  Last test is failing
             console.log(result);
 		return result;
 		// should take a string of any length, and check the matching objects from animals.json
 		// and return an array of 5 strings
 	},
 
-	search: query => {
-		return {};
+	searchResult: query => {
 		// take in a query (string) and return one matching result (object)
 	}
 };
