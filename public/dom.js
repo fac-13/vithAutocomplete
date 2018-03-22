49a3de7dfe9e49a2916d74a97ab8f9a2a915f69d
tdoran <tomgdoran@gmail.com>
2018-03-22 11:03:17 +0000

    refactored keyup-listener to handle various edgecases, more styling on dropdown list Relates #6 #4 #9

--- public/dom.js
+++ public/dom.js
@@ -1,31 +1,43 @@
 (function () {
 	document.querySelector('.form__input').focus(); // gives immediate focus to the form
 	var domList = document.querySelector('#js-result');
-	// var input = document.querySelector('#js-input');
-	var url = '/search/?q='; // function scoped variable for the search string
+	var domInput = document.querySelector('#js-input');
+	var startURL = '/search/?q='; // function scoped variable for the search string
+	var height = document.getElementById('js-submit').offsetHeight;
+	var inputString = '';
+	console.log(height);
 
 	// ADD LISTENER TO COMBINE IT ALL TOGETHER
 	request.addListener('#js-submit', 'keyup', function (event) {
 		event.preventDefault();
+		inputString = domInput.value;
 		//STETCH: if statement to handle highlight deletion
 		// Creates our Search String
-		var input = event.key;
-		if (event.keyCode >= 48 && event.keyCode <= 90) { // Do we care about punctuation and add more key codes ie space bar? (stretch!!!)
-			url = url += input;
-			request.fetch(url, displaySuggestions);
-
-		} else if (event.keyCode === 8) {
-			url = url.substring(0, url.length - 1);
-			request.fetch(url, displaySuggestions);
-
+		if (inputString === '') {
+			removeChildren();
 		}
+		if (event.keyCode >= 48 && event.keyCode <= 90) { 
+			// Do we care about punctuation and add more key codes ie space bar? (stretch!!!)
+			if (inputString !== '') {
+				request.fetch(startURL + inputString, displaySuggestions);
+			}
+
+		} else if (event.keyCode === 8 && inputString !== '') {
+			// on backspace
+			request.fetch(startURL + inputString, displaySuggestions);
+		}
+		console.log('domInputvalie', domInput.value);
+		console.log('inputString', inputString);
 	});
 
-	// FUNCTION FOR DOM MANIPULATION ONCE RECEIVED REQUEST
-	function displaySuggestions(response) {
+	function removeChildren() {
 		while (domList.firstChild) {
 			domList.removeChild(domList.firstChild); // refreshes tracklist for repeadted searches
-		  }
+		}
+	}
+	// FUNCTION FOR DOM MANIPULATION ONCE RECEIVED REQUEST
+	function displaySuggestions(response) {
+		removeChildren();
 		response.forEach(function (item) {
 			var domItem = document.createElement("li");
 			domItem.classList.add("result__item");
